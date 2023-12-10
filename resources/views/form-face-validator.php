b1431e5594f421ca280e3de2025173a95847e35b
ragamedicaboo <github.ragamulia@gmail.com>
2023-12-10 17:25:55 +0700

    fix: set viewport form face validator

--- resources/views/form-face-validator.php
+++ resources/views/form-face-validator.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
 <style>
     .body {
         height : 100vh;
@@ -124,3 +133,7 @@
         }
     }
 </script>
+
+
+</body>
+</html>
